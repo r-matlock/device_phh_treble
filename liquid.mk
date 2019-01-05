04091f2ce17aec010a325f0b916ce60260447ec3
Raleigh Matlock <raleighman2@gmail.com>
2019-01-04 22:17:02 -0500

    Update liquid.mk
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- liquid.mk
+++ liquid.mk
@@ -1,3 +1,3 @@
 $(call inherit-product, device/liquid/sepolicy/common/sepolicy.mk)
 $(call inherit-product, vendor/liquid/config/common_full_phone.mk)
--include vendor/lineage/build/core/config.mk
+-include vendor/liquid/build/core/config.mk
